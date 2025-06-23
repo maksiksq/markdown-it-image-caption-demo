dcf7f87293b9b407f009e6fea7353dfc6607afd8
Maksiks <maksiksking@gmail.com>
2025-06-23 17:46:52 +0300

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <section>
+        <p>write something in here</p>
+        <textarea></textarea>
+    </section>
+</main>
+
+<style>
+    :global {
+        body {
+            background-color: #ffd58c;
+            overflow: clip;
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+        }
+    }
+
+    main {
+        width: 100vw;
+        height: 100vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        section {
+            display: flex;
+            flex-direction: column;
+
+            p {
+                font-weight: bold;
+                font-size: 1.2rem;
+                font-family: Comfortaa, sans-serif;
+            }
+
+            textarea {
+                width: 100%;
+            }
+        }
+    }
+</style>
