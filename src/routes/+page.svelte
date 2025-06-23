a9c5645eb216770027ba63e463e2595798fca3e0
Maksiks <maksiksking@gmail.com>
2025-06-23 18:41:55 +0300

    feat: lmao this gif is flawless

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,12 @@
 </script>
 
 <main>
-    <section>
-        <p>✏️ write something in here</p>
+    <section class="edit">
+        <p>✏️ Write something in here</p><br>
+        <p class="small">For example: <br> ![test](https://i.pinimg.com/originals/0b/12/8a/0b128adee2c032cd1ab8d7d970917361.gif 'Rendered 🎉!')</p>
         <textarea bind:value={textBoxContent}></textarea>
-
+    </section>
+    <section class="markdown">
         <p>{@html text}</p>
     </section>
 </main>
@@ -22,12 +24,15 @@
     :global {
         body {
             background-color: #ffd58c;
-            overflow: clip;
+            overflow-x: clip;
+            overflow-y: auto;
         }
 
         * {
             margin: 0;
             padding: 0;
+
+            font-family: Comfortaa, sans-serif;
         }
     }
 
@@ -36,25 +41,51 @@
         height: 100vh;
 
         display: flex;
-        justify-content: center;
+        flex-direction: column;
         align-items: center;
 
-        section {
+        & .edit {
+            margin-top: 100px;
             display: flex;
             flex-direction: column;
+            width: 40vw;
 
-            p {
+            & p {
                 font-weight: bold;
                 font-size: 1.2rem;
-                font-family: Comfortaa, sans-serif;
             }
 
-            textarea {
+            & .small {
+                font-size: 0.8rem;
+            }
+
+            & textarea {
                 margin-top: 10px;
 
-                width: 40vw;
-                height: 50vh;
+                width: 100%;
+                height: 20vh;
             }
         }
+
+        & .markdown {
+            width: 60%;
+
+            & p {
+                margin-top: 50px;
+                font-weight: bold;
+                font-size: 1rem;
+
+                padding-bottom: 50px;
+
+                :global {
+                    figure {
+                        & img {
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 </style>
